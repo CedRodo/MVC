5e6e6bd98e22d49888e4ce7d05b7df1eb3e0d6c2
Cédric Rodony <cedric.rodony@gmail.com>
2022-02-20 05:13:18 +0100

    modifs favoris principalement

--- public/index.php
+++ public/index.php
@@ -1,20 +1,22 @@
 <?php
 // routeur : 
 //dans cd public
-// php -S localhost:8080
+// php -S localhost:8000
 
 if (isset($_SERVER['PATH_INFO'])==false){
   
     include __DIR__.'/../src/controller/homeController.php';
-   // echo "vous êtes à la page d'accueil";
-
 }
+
 else if ($_SERVER['PATH_INFO']=="/liste_articles"){
    
     include __DIR__.'/../src/controller/liste_artController.php';
 
 }
 
+else if ($_SERVER['PATH_INFO']=="/article"){
+    include __DIR__.'/../src/controller/articleController.php';
+}
 
 else if ($_SERVER['PATH_INFO']=="/creer_article"){
     include __DIR__.'/../src/controller/insertion_artController.php';
@@ -36,10 +38,33 @@ else if ($_SERVER['PATH_INFO']=="/suppression_article"){
     include __DIR__.'/../src/controller/supp_artController.php';
 }
 
+else if ($_SERVER['PATH_INFO']=="/ajouter_favoris"){
+    include __DIR__.'/../src/controller/ajout_favController.php';
+}
+
 else if ($_SERVER['PATH_INFO']=="/favoris_articles"){
     include __DIR__.'/../src/controller/fav_artController.php';
 }
 
-// EXO CREEZ un nouveau projet avec un routeur dans le dossier public
-// avec plusieurs page 2 ou 3 
-// avec le serveur lancé depuis le terminal
+else if ($_SERVER['PATH_INFO']=="/retirer_favori"){
+    include __DIR__.'/../src/controller/retir_favController.php';
+}
+
+else if ($_SERVER['PATH_INFO']=="/vider_favoris"){
+    include __DIR__.'/../src/controller/vider_favController.php';
+}
+
+else if ($_SERVER['PATH_INFO']=="/ajouter_panier"){
+    include __DIR__.'/../src/controller/ajout_panController.php';
+}
+
+
+else if ($_SERVER['PATH_INFO']=="/panier_articles"){
+    include __DIR__.'/../src/controller/pan_artController.php';
+}
+
+else if ($_SERVER['PATH_INFO']=="/add_cart"){
+   
+    include __DIR__.'/../src/controller/CartController.php';
+}
+
